e4d342d9c6af1f79dd01830fdab8f8e16c7f7eb8
SrVasr <vasr.main@gmail.com>
2022-02-17 03:42:08 +0100

    Beta 2.0

--- config.sh
+++ config.sh
@@ -22,6 +22,8 @@ printf "${BgPurple}${BdGreen}[+] Installing all the dependencies${Reset}\n"
 sudo apt update
 sudo apt-get install build-essential git vim xcb libxkbcommon-dev python3 libfontconfig1-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev libpam0g-dev libxrandr-dev libfreetype6-dev libimlib2-dev libxft-dev libuv1-dev python3-setuptools gettext
 sudo parrot-upgrade
+sudo apt autoremove
+sudo apt autoclean
 
 sudo cp -R fonts /usr/local/share/
 sudo cp -R icons /usr/share/
